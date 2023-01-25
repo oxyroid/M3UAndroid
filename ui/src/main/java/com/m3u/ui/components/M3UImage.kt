dd9245d65115722d216b466627030ae3208cf5ec
sortBy <sortby@qq.com>
2023-01-26 02:36:11 +0800

    feat: buttons

---
+++ ui/src/main/java/com/m3u/ui/components/M3UImage.kt
@@ -0,0 +1,62 @@
+package com.m3u.ui.components
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import coil.compose.SubcomposeAsyncImage
+import com.m3u.ui.R
+import com.m3u.ui.components.basic.M3UBox
+import com.m3u.ui.local.LocalTheme
+
+@Composable
+fun M3UImage(
+    model: Any?,
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    contentScale: ContentScale = ContentScale.Fit
+) {
+    Surface(
+        shape = MaterialTheme.shapes.medium
+    ) {
+        SubcomposeAsyncImage(
+            model = model,
+            contentDescription = contentDescription,
+            modifier = modifier,
+            contentScale = contentScale,
+            loading = {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    shape = MaterialTheme.shapes.medium,
+                    color = LocalTheme.current.surface
+                ) {
+
+                }
+            },
+            error = {
+                Surface(
+                    shape = MaterialTheme.shapes.medium,
+                    color = LocalTheme.current.error,
+                ) {
+                    M3UBox(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        val message =
+                            it.result.throwable.message ?: stringResource(R.string.error_unknown)
+                        Text(
+                            text = message,
+                            style = MaterialTheme.typography.h6,
+                            color = LocalTheme.current.onError
+                        )
+                    }
+                }
+            }
+        )
+    }
+}
