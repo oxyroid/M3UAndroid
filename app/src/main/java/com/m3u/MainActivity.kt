3f0bf006e2ca8496fa746ca7a2b7f99870a37c54
sortBy <sortby@qq.com>
2023-01-29 00:55:57 +0800

    feat: splash screen

--- app/src/main/java/com/m3u/MainActivity.kt
+++ app/src/main/java/com/m3u/MainActivity.kt
@@ -3,13 +3,17 @@ package com.m3u
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import com.m3u.ui.M3UApp
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             M3UApp()
         }
