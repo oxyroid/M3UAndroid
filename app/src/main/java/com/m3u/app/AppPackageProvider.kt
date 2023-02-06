b3f45d728e0576124ae8e6f189ae7a7f11a7cf89
sortBy <sortby@qq.com>
2023-02-06 16:26:52 +0800

    upgrade: 1.8.0-alpha01

--- app/src/main/java/com/m3u/app/AppPackageProvider.kt
+++ app/src/main/java/com/m3u/app/AppPackageProvider.kt
@@ -3,18 +3,8 @@ package com.m3u.app
 import com.m3u.core.architecture.PackageProvider
 import javax.inject.Inject
 
-class AppPackageProvider @Inject constructor(
-
-) : PackageProvider {
-    override fun getApplicationID(): String {
-        return BuildConfig.APPLICATION_ID
-    }
-
-    override fun getVersionName(): String {
-        return BuildConfig.VERSION_NAME
-    }
-
-    override fun isDebug(): Boolean {
-        return BuildConfig.DEBUG
-    }
+class AppPackageProvider @Inject constructor() : PackageProvider {
+    override fun getApplicationID(): String = BuildConfig.APPLICATION_ID
+    override fun getVersionName(): String = BuildConfig.VERSION_NAME
+    override fun isDebug(): Boolean = BuildConfig.DEBUG
 }
