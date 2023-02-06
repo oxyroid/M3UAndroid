dd6651f2a493cb8404197a41e1d19956ee049572
sortBy <sortby@qq.com>
2023-02-06 16:25:24 +0800

    refactor: setting interface

--- features/setting/src/main/java/com/m3u/features/setting/SettingScreen.kt
+++ features/setting/src/main/java/com/m3u/features/setting/SettingScreen.kt
@@ -1,21 +1,26 @@
 package com.m3u.features.setting
 
 import android.content.res.Configuration
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.rememberScrollableState
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -23,6 +28,7 @@ import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -33,9 +39,9 @@ import com.m3u.core.annotation.SetSyncMode
 import com.m3u.core.annotation.SyncMode
 import com.m3u.core.util.context.toast
 import com.m3u.features.setting.components.CheckBoxPreference
+import com.m3u.features.setting.components.FoldPreference
 import com.m3u.features.setting.components.TextPreference
 import com.m3u.ui.components.M3UColumn
-import com.m3u.ui.components.M3URow
 import com.m3u.ui.components.M3UTextButton
 import com.m3u.ui.components.M3UTextField
 import com.m3u.ui.model.AppAction
@@ -85,7 +91,7 @@ internal fun SettingRoute(
         onSyncMode = { viewModel.onEvent(SettingEvent.OnSyncMode(it)) },
         useCommonUIMode = state.useCommonUIMode,
         onUIMode = { viewModel.onEvent(SettingEvent.OnUIMode) },
-        modifier = modifier
+        modifier = modifier.fillMaxSize()
     )
 }
 
@@ -104,82 +110,50 @@ private fun SettingScreen(
     onUIMode: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val spacing = LocalSpacing.current
-    Column(
-        modifier = Modifier
-            .scrollable(
-                orientation = Orientation.Vertical,
-                state = rememberScrollableState { it }
-            )
-            .fillMaxSize()
-            .testTag("features:setting")
+    Box(
+        modifier = Modifier.testTag("features:setting")
     ) {
         val configuration = LocalConfiguration.current
         when (configuration.orientation) {
             Configuration.ORIENTATION_PORTRAIT -> {
-                M3UColumn(
-                    verticalArrangement = Arrangement.spacedBy(spacing.small),
+                PortraitOrientationContent(
+                    title = title,
+                    url = url,
+                    subscribeEnable = subscribeEnable,
+                    onTitle = onTitle,
+                    onUrl = onUrl,
+                    onSubscribe = onSubscribe,
+                    syncMode = syncMode,
+                    onSyncMode = onSyncMode,
+                    version = version,
                     modifier = modifier
-                ) {
-                    MakeSubscriptionPart(
-                        title = title,
-                        url = url,
-                        subscribeEnable = subscribeEnable,
-                        onTitle = onTitle,
-                        onUrl = onUrl,
-                        onSubscribe = onSubscribe,
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                    SettingItemsPart(
-                        syncMode = syncMode,
-                        onSyncMode = onSyncMode,
-                        useCommonUIMode = useCommonUIMode,
-                        onUIMode = onUIMode,
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                    M3UTextButton(
-                        text = stringResource(R.string.label_app_version, version),
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
-                    ) {
-
-                    }
-                }
+                        .fillMaxWidth()
+                        .scrollable(
+                            orientation = Orientation.Vertical,
+                            state = rememberScrollableState { it }
+                        )
+                )
             }
 
             Configuration.ORIENTATION_LANDSCAPE -> {
-                M3URow(
-                    horizontalArrangement = Arrangement.spacedBy(spacing.medium),
-                    modifier = modifier.padding(horizontal = spacing.medium)
-                ) {
-                    MakeSubscriptionPart(
-                        title = title,
-                        url = url,
-                        subscribeEnable = subscribeEnable,
-                        onTitle = onTitle,
-                        onUrl = onUrl,
-                        onSubscribe = onSubscribe,
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .weight(1f)
-                    )
-                    Column(
-                        modifier = modifier.weight(1f)
-                    ) {
-                        SettingItemsPart(
-                            syncMode = syncMode,
-                            onSyncMode = onSyncMode,
-                            useCommonUIMode = useCommonUIMode,
-                            onUIMode = onUIMode,
-                            modifier = Modifier.fillMaxWidth()
+                LandscapeOrientationContent(
+                    title = title,
+                    url = url,
+                    subscribeEnable = subscribeEnable,
+                    onTitle = onTitle,
+                    onUrl = onUrl,
+                    onSubscribe = onSubscribe,
+                    syncMode = syncMode,
+                    onSyncMode = onSyncMode,
+                    useCommonUIMode = useCommonUIMode,
+                    onUIMode = onUIMode,
+                    version = version,
+                    modifier = modifier
+                        .scrollable(
+                            orientation = Orientation.Vertical,
+                            state = rememberScrollableState { it }
                         )
-                        M3UTextButton(
-                            text = stringResource(R.string.label_app_version, version),
-                            modifier = Modifier.align(Alignment.CenterHorizontally)
-                        ) {
-
-                        }
-                    }
-                }
+                )
             }
 
             else -> {}
@@ -188,7 +162,201 @@ private fun SettingScreen(
 }
 
 @Composable
-private fun MakeSubscriptionPart(
+private fun PortraitOrientationContent(
+    title: String,
+    url: String,
+    subscribeEnable: Boolean,
+    onTitle: (String) -> Unit,
+    onUrl: (String) -> Unit,
+    onSubscribe: () -> Unit,
+    syncMode: @SyncMode Int,
+    onSyncMode: SetSyncMode,
+    version: String,
+    modifier: Modifier = Modifier
+) {
+    var fold: Fold by remember { mutableStateOf(Fold.NONE) }
+    when (fold) {
+        Fold.NONE -> {
+            PreferencesPart(
+                version = version,
+                onSubscriptionManagement = {
+                    fold = Fold.SUBSCRIPTION
+                },
+                onScriptManagement = {
+                    fold = Fold.SCRIPT
+                },
+                syncMode = syncMode,
+                onSyncMode = onSyncMode,
+                useCommonUIMode = true,
+                useCommonUIModeEnable = false,
+                onUIMode = { },
+                modifier = modifier
+            )
+        }
+
+        Fold.SUBSCRIPTION -> {
+            SubscriptionManagementPart(
+                title = title,
+                url = url,
+                subscribeEnable = subscribeEnable,
+                onTitle = onTitle,
+                onUrl = onUrl,
+                onSubscribe = onSubscribe,
+                modifier = modifier
+            )
+        }
+
+        Fold.SCRIPT -> {
+            ScriptManagementPart(
+                modifier = modifier
+            )
+        }
+    }
+
+    BackHandler(fold != Fold.NONE) {
+        fold = Fold.NONE
+    }
+}
+
+@Composable
+private fun LandscapeOrientationContent(
+    title: String,
+    url: String,
+    subscribeEnable: Boolean,
+    onTitle: (String) -> Unit,
+    onUrl: (String) -> Unit,
+    onSubscribe: () -> Unit,
+    syncMode: @SyncMode Int,
+    onSyncMode: SetSyncMode,
+    useCommonUIMode: Boolean,
+    onUIMode: () -> Unit,
+    version: String,
+    modifier: Modifier = Modifier
+) {
+    val spacing = LocalSpacing.current
+    var fold: Fold by remember { mutableStateOf(Fold.NONE) }
+    fun setFold(target: Fold) {
+        fold = (if (fold == target) Fold.NONE else target)
+    }
+
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(spacing.medium, Alignment.Start),
+        modifier = modifier.padding(horizontal = spacing.medium)
+    ) {
+        PreferencesPart(
+            version = version,
+            onSubscriptionManagement = { setFold(Fold.SUBSCRIPTION) },
+            onScriptManagement = { setFold(Fold.SCRIPT) },
+            syncMode = syncMode,
+            onSyncMode = onSyncMode,
+            useCommonUIMode = useCommonUIMode,
+            onUIMode = onUIMode,
+            modifier = Modifier
+                .fillMaxHeight()
+                .weight(1f)
+        )
+
+        when (fold) {
+            Fold.SUBSCRIPTION -> {
+                SubscriptionManagementPart(
+                    title = title,
+                    url = url,
+                    subscribeEnable = subscribeEnable,
+                    onTitle = onTitle,
+                    onUrl = onUrl,
+                    onSubscribe = onSubscribe,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .weight(1f)
+                )
+            }
+
+            Fold.SCRIPT -> {
+                ScriptManagementPart(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .weight(1f)
+                )
+            }
+
+            else -> {}
+        }
+    }
+    BackHandler(fold != Fold.NONE) {
+        fold = Fold.NONE
+    }
+}
+
+@Composable
+private fun PreferencesPart(
+    version: String,
+    onSubscriptionManagement: () -> Unit,
+    onScriptManagement: () -> Unit,
+    syncMode: @SyncMode Int,
+    onSyncMode: SetSyncMode,
+    useCommonUIMode: Boolean,
+    onUIMode: () -> Unit,
+    modifier: Modifier = Modifier,
+    useCommonUIModeEnable: Boolean = true,
+) {
+    M3UColumn(
+        modifier = modifier
+    ) {
+        Column(
+            modifier = Modifier.clip(RoundedCornerShape(LocalSpacing.current.medium)),
+            verticalArrangement = Arrangement.spacedBy(1.dp)
+        ) {
+            FoldPreference(
+                title = stringResource(R.string.subscription_management),
+                enabled = true,
+                onClick = onSubscriptionManagement
+            )
+            FoldPreference(
+                title = stringResource(R.string.script_management),
+                enabled = true,
+                onClick = onScriptManagement
+            )
+            TextPreference(
+                title = stringResource(R.string.sync_mode),
+                content = when (syncMode) {
+                    SyncMode.DEFAULT -> stringResource(R.string.sync_mode_default)
+                    SyncMode.EXCEPT -> stringResource(R.string.sync_mode_favourite_except)
+                    else -> ""
+                },
+                onClick = {
+                    // TODO
+                    val target = when (syncMode) {
+                        SyncMode.DEFAULT -> SyncMode.EXCEPT
+                        else -> SyncMode.DEFAULT
+                    }
+                    onSyncMode(target)
+                }
+            )
+            CheckBoxPreference(
+                title = stringResource(R.string.common_ui_mode),
+                subtitle = stringResource(R.string.common_ui_mode_description),
+                enabled = useCommonUIModeEnable,
+                checked = useCommonUIMode,
+                onCheckedChange = { newValue ->
+                    if (newValue != useCommonUIMode) {
+                        onUIMode()
+                    }
+                }
+            )
+        }
+        val uriHandler = LocalUriHandler.current
+        M3UTextButton(
+            text = stringResource(R.string.label_app_version, version),
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            val url = "https://github.com/thxbrop/M3UAndroid/releases/tag/v$version"
+            uriHandler.openUri(url)
+        }
+    }
+}
+
+@Composable
+private fun SubscriptionManagementPart(
     title: String,
     url: String,
     subscribeEnable: Boolean,
@@ -197,7 +365,7 @@ private fun MakeSubscriptionPart(
     onSubscribe: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Column(
+    M3UColumn(
         verticalArrangement = Arrangement.spacedBy(LocalSpacing.current.small),
         modifier = modifier
     ) {
@@ -234,48 +402,20 @@ private fun MakeSubscriptionPart(
             onClick = { onSubscribe() },
             modifier = Modifier.fillMaxWidth()
         )
-
     }
 }
 
 @Composable
-private fun SettingItemsPart(
-    syncMode: @SyncMode Int,
-    onSyncMode: SetSyncMode,
-    useCommonUIMode: Boolean,
-    onUIMode: () -> Unit,
+fun ScriptManagementPart(
     modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = modifier.clip(MaterialTheme.shapes.medium),
-        verticalArrangement = Arrangement.spacedBy(1.dp)
+    M3UColumn(
+        modifier = modifier
     ) {
-        TextPreference(
-            title = stringResource(R.string.sync_mode),
-            content = when (syncMode) {
-                SyncMode.DEFAULT -> stringResource(R.string.sync_mode_default)
-                SyncMode.EXCEPT -> stringResource(R.string.sync_mode_favourite_except)
-                else -> ""
-            },
-            onClick = {
-                // TODO
-                val target = when (syncMode) {
-                    SyncMode.DEFAULT -> SyncMode.EXCEPT
-                    else -> SyncMode.DEFAULT
-                }
-                onSyncMode(target)
-            }
-        )
-        CheckBoxPreference(
-            title = stringResource(R.string.common_ui_mode),
-            subtitle = stringResource(R.string.common_ui_mode_description),
-            enabled = true,
-            checked = useCommonUIMode,
-            onCheckedChange = { newValue ->
-                if (newValue != useCommonUIMode) {
-                    onUIMode()
-                }
-            }
-        )
+
     }
+}
+
+private enum class Fold {
+    NONE, SUBSCRIPTION, SCRIPT
 }
