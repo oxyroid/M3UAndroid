3a059979991a4ece46a0053fcaae131fba525bff
thxbrop <1365427005@qq.com>
2023-02-18 00:24:26 +0800

    fix: remove dialog after the list dialog

--- features/feed/src/main/java/com/m3u/features/feed/components/FeedDialog.kt
+++ features/feed/src/main/java/com/m3u/features/feed/components/FeedDialog.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.window.Dialog
 import com.m3u.data.entity.Live
 import com.m3u.features.feed.R
-import com.m3u.ui.components.AlertDialog
 import com.m3u.ui.model.LocalSpacing
 import com.m3u.ui.model.LocalTheme
 
@@ -30,60 +29,6 @@ internal fun FeedDialog(
     onSavePicture: (Int) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val title = when (state) {
-        DialogState.Idle -> ""
-        is DialogState.Menu -> ""
-        is DialogState.Favorite -> if (state.live.favourite) stringResource(R.string.dialog_favourite_cancel_title)
-        else stringResource(R.string.dialog_favourite_title)
-        is DialogState.Mute -> stringResource(R.string.dialog_mute_title)
-        is DialogState.SavePicture -> ""
-    }
-
-    val content = when (state) {
-        DialogState.Idle -> ""
-        is DialogState.Menu -> ""
-        is DialogState.Favorite -> stringResource(R.string.dialog_favourite_content)
-        is DialogState.Mute -> stringResource(R.string.dialog_mute_content)
-        is DialogState.SavePicture -> ""
-    }
-    val confirm = when (state) {
-        DialogState.Idle -> null
-        is DialogState.Menu -> null
-        is DialogState.Favorite -> stringResource(R.string.dialog_favourite_confirm)
-        is DialogState.Mute -> stringResource(R.string.dialog_mute_confirm)
-        is DialogState.SavePicture -> null
-    }
-    val dismiss = when (state) {
-        DialogState.Idle -> null
-        is DialogState.Menu -> null
-        is DialogState.Favorite -> stringResource(R.string.dialog_favourite_dismiss)
-        is DialogState.Mute -> stringResource(R.string.dialog_mute_dismiss)
-        is DialogState.SavePicture -> null
-    }
-
-    fun onConfirm() = when (state) {
-        DialogState.Idle -> {}
-        is DialogState.Menu -> {}
-        is DialogState.Favorite -> {
-            onUpdate(DialogState.Idle)
-            onFavorite(state.live.id, !state.live.favourite)
-        }
-        is DialogState.Mute -> {
-            onUpdate(DialogState.Idle)
-            // TODO
-            onMute(state.live.id, true)
-        }
-        is DialogState.SavePicture -> {}
-    }
-
-    fun onDismiss() = when (state) {
-        DialogState.Idle -> {}
-        is DialogState.Menu -> onUpdate(DialogState.Idle)
-        is DialogState.Favorite -> onUpdate(DialogState.Menu(state.live))
-        is DialogState.Mute -> onUpdate(DialogState.Menu(state.live))
-        is DialogState.SavePicture -> {}
-    }
-
     when (state) {
         DialogState.Idle -> {}
         is DialogState.Menu -> {
@@ -96,42 +41,34 @@ internal fun FeedDialog(
                     modifier = modifier
                 ) {
                     Column {
+                        val favourite = state.live.favourite
                         DialogItem(
-                            resId = if (state.live.favourite) R.string.dialog_favourite_cancel_title
+                            resId = if (favourite) R.string.dialog_favourite_cancel_title
                             else R.string.dialog_favourite_title,
                             onUpdate = {
-                                if (state.live.favourite) {
-                                    onUpdate(DialogState.Idle)
-                                    onFavorite(state.live.id, false)
-                                } else {
-                                    onUpdate(DialogState.Favorite(state.live))
-                                }
+                                onUpdate(DialogState.Idle)
+                                onFavorite(state.live.id, !favourite)
                             },
                         )
                         DialogItem(
                             resId = R.string.dialog_mute_title,
-                            onUpdate = { onUpdate(DialogState.Mute(state.live)) },
+                            onUpdate = {
+                                onUpdate(DialogState.Idle)
+                                // TODO
+                                onMute(state.live.id, true)
+                            },
                         )
 
-                        DialogItem(resId = R.string.dialog_save_picture_title, onUpdate = {
-                            onUpdate(DialogState.Idle)
-                            onSavePicture(state.live.id)
-                        })
+                        DialogItem(
+                            resId = R.string.dialog_save_picture_title,
+                            onUpdate = {
+                                onUpdate(DialogState.Idle)
+                                onSavePicture(state.live.id)
+                            }
+                        )
                     }
                 }
             }
-        }
-        else -> {
-            AlertDialog(
-                title = title,
-                text = content,
-                confirm = confirm,
-                dismiss = dismiss,
-                onDismissRequest = { onUpdate(DialogState.Idle) },
-                onConfirm = ::onConfirm,
-                onDismiss = ::onDismiss,
-                modifier = modifier
-            )
         }
     }
 }
@@ -139,9 +76,6 @@ internal fun FeedDialog(
 internal sealed class DialogState {
     object Idle : DialogState()
     data class Menu(val live: Live) : DialogState()
-    data class Favorite(val live: Live) : DialogState()
-    data class Mute(val live: Live) : DialogState()
-    data class SavePicture(val live: Live) : DialogState()
 }
 
 @Composable
