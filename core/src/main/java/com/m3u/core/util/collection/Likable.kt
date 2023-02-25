f99680ef5ad3bb09c7a6f53b706ddfa7784bd853
thxbrop <1365427005@qq.com>
2023-02-26 01:39:43 +0800

    feat: notlike notbelong nothold

--- core/src/main/java/com/m3u/core/util/collection/Likable.kt
+++ core/src/main/java/com/m3u/core/util/collection/Likable.kt
@@ -6,6 +6,10 @@ interface Likable<T> {
     infix fun like(another: T): Boolean = this == another
 }
 
+
+infix fun <T : Likable<T>> T.notlike(another: T): Boolean = this.like(another).not()
 infix fun <T : Likable<T>> T.belong(c: Collection<T>): Boolean = c.any { it like this }
+infix fun <T : Likable<T>> T.notbelong(c: Collection<T>): Boolean = c.all { it notlike this }
 
 infix fun <T : Likable<T>> Collection<T>.hold(t: T): Boolean = this.any { it like t }
+infix fun <T : Likable<T>> Collection<T>.nothold(t: T): Boolean = this.all { it notlike t }
