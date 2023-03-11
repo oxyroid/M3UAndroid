3e464a7a74bc41fa9bd3b059a6cc94ea481d613a
thxbrop <1365427005@qq.com>
2023-03-11 14:12:55 +0800

    fix: remove SharedScope implementation in Destination

--- features/crash/src/main/java/com/m3u/features/crash/navigation/Destination.kt
+++ features/crash/src/main/java/com/m3u/features/crash/navigation/Destination.kt
@@ -1,31 +1,6 @@
 package com.m3u.features.crash.navigation
 
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalConfiguration
-import com.m3u.ui.shared.SharedState
-import com.m3u.ui.shared.SharedScope
-import com.m3u.ui.shared.sharedState
-
-internal sealed class Destination : SharedScope {
-    object List : Destination() {
-        override val isElement: Boolean = false
-        override val state: SharedState
-            @Composable get() = LocalConfiguration.current.sharedState
-
-        @Suppress("UNCHECKED_CAST")
-        override fun <T : SharedScope> copy(state: SharedState): T = this as T
-    }
-
-    data class Detail(
-        override val state: SharedState,
-        val path: String
-    ) : Destination() {
-        override val isElement: Boolean = true
-
-        @Suppress("UNCHECKED_CAST")
-        override fun <T : SharedScope> copy(state: SharedState): T = copy(
-            state = state,
-            path = path
-        ) as T
-    }
+internal sealed class Destination {
+    object List : Destination()
+    data class Detail(val path: String) : Destination()
 }
