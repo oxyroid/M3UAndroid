df598b70fe9386529aea0fb50210e6d755ef68d5
thxbrop <1365427005@qq.com>
2023-03-13 23:45:33 +0800

    style: badge

--- ui/src/main/java/com/m3u/ui/components/Badge.kt
+++ ui/src/main/java/com/m3u/ui/components/Badge.kt
@@ -1,11 +1,14 @@
 package com.m3u.ui.components
 
-import androidx.compose.material.Badge
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import com.m3u.ui.model.LocalSpacing
 import com.m3u.ui.model.LocalTheme
 
 @Composable
@@ -17,16 +20,21 @@ fun TextBadge(
     icon: (@Composable () -> Unit)? = null,
 ) {
     val theme = LocalTheme.current
-    Badge(
+    val spacing = LocalSpacing.current
+    Surface(
         modifier = modifier,
-        backgroundColor = color,
-        contentColor = contentColor
+        color = color,
+        contentColor = contentColor,
+        shape = RoundedCornerShape(spacing.medium)
     ) {
         icon?.invoke()
         Text(
             text = text.uppercase(),
             style = MaterialTheme.typography.subtitle2,
-            color = theme.onTint
+            color = theme.onTint,
+            modifier = Modifier.padding(
+                horizontal = spacing.extraSmall
+            )
         )
     }
 }
