a63c5eeeaaf5e8dfe5c13ae541fc99ecd5d9a8e6
thxbrop <1365427005@qq.com>
2023-04-02 20:02:04 +0800

    feat: scheme

---
+++ features/scheme/src/main/java/com/m3u/features/scheme/SchemeReceiverService.kt
@@ -0,0 +1,25 @@
+package com.m3u.features.scheme
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import com.m3u.core.architecture.Logger
+import com.m3u.core.util.context.toast
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class SchemeReceiverService : Service() {
+    @Inject
+    lateinit var logger: Logger
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val url = intent?.data?.toString()
+        applicationContext.toast(url.orEmpty())
+        return START_NOT_STICKY
+    }
+}
