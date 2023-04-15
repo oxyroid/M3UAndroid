b818c48b2c62f3dca9f78d8526da660862a6e0f8
thxbrop <1365427005@qq.com>
2023-04-15 14:55:41 +0800

    build: shared scope api has been removed

--- ui/src/main/java/com/m3u/ui/shared/material/SharedSurface.kt
+++ ui/src/main/java/com/m3u/ui/shared/material/SharedSurface.kt
@@ -2,16 +2,10 @@ package com.m3u.ui.shared.material
 
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.m3u.ui.model.LocalTheme
-import com.m3u.ui.shared.SharedElementScope
-import com.m3u.ui.shared.SharedScope
 import com.m3u.ui.shared.SharedState
-import com.m3u.ui.shared.animation.AnimatedModifier
 import com.m3u.ui.shared.sharedElement
 
 @Composable
@@ -40,46 +34,4 @@ object SharedHostDefaults {
 
     @Composable
     fun surfaceContentColor(): Color = LocalTheme.current.onBackground
-}
-
-
-/**
- * A shared element surface.
- *
- * @param scope The current [SharedScope]
- * @param backgroundContent The root content.
- * @param foregroundContent The content which is both as shared element and font content.
- * @param onStart The callback when the [foregroundContent] should be started.
- * @see SharedScope
- * @suppress The [backgroundContent]'s lifecycle will not enter PAUSE or later state when it is dismiss.
- */
-@Composable
-@Deprecated("Use ShareSurface(state: SharedState,...) instead")
-fun SharedSurface(
-    scope: SharedScope,
-    backgroundContent: @Composable () -> Unit,
-    foregroundContent: @Composable SharedElementScope.() -> Unit,
-    modifier: Modifier = Modifier,
-    onStart: () -> Unit = {},
-    color: Color = SharedHostDefaults.surfaceColor(),
-    contentColor: Color = SharedHostDefaults.surfaceContentColor(),
-) {
-    val currentOnStart by rememberUpdatedState(onStart)
-    Surface(
-        color = color,
-        contentColor = contentColor
-    ) {
-        backgroundContent()
-        if (scope is SharedElementScope) {
-            AnimatedModifier(
-                state = scope.state,
-                modifier = modifier
-            ) {
-                foregroundContent(scope)
-            }
-            LaunchedEffect(scope) {
-                currentOnStart()
-            }
-        }
-    }
 }
