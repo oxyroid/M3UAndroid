fa5708f0cf7d5ede30925fb56cfe032ea1303eed
thxbrop <1365427005@qq.com>
2023-04-21 23:05:51 +0800

    fix: multiple click to crash

--- features/feed/src/main/java/com/m3u/features/feed/components/LiveItem.kt
+++ features/feed/src/main/java/com/m3u/features/feed/components/LiveItem.kt
@@ -16,7 +16,10 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -60,10 +63,17 @@ internal fun LiveItem(
         contentColor = theme.onSurface,
         elevation = spacing.none
     ) {
+        var lastClick by remember { mutableStateOf(0L) }
         Column(
             modifier = Modifier
                 .combinedClickable(
-                    onClick = onClick,
+                    onClick = {
+                        val current = System.currentTimeMillis()
+                        if (current - lastClick >= 800L) {
+                            onClick()
+                            lastClick = current
+                        }
+                    },
                     onLongClick = onLongClick
                 )
                 .then(modifier)
