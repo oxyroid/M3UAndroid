3c0d3582ff506dc8d89261c97998657483d06ce0
thxbrop <1365427005@qq.com>
2023-04-30 03:55:18 +0800

    style: update crash screen status bar style

--- features/crash/src/main/java/com/m3u/features/crash/CrashActivity.kt
+++ features/crash/src/main/java/com/m3u/features/crash/CrashActivity.kt
@@ -3,6 +3,10 @@ package com.m3u.features.crash
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.m3u.ui.M3ULocalProvider
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,6 +16,16 @@ class CrashActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             M3ULocalProvider {
+                val systemUiController = rememberSystemUiController()
+                val useDarkIcons = isSystemInDarkTheme()
+
+                DisposableEffect(systemUiController, useDarkIcons) {
+                    systemUiController.setSystemBarsColor(
+                        color = Color.Transparent,
+                        darkIcons = useDarkIcons
+                    )
+                    onDispose {}
+                }
                 CrashApp()
             }
         }
