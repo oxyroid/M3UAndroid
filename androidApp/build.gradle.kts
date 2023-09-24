2cfca4a8b86c9886578dd7c4e8b9486af984e050
oxy <sortby@qq.com>
2023-09-24 18:22:58 +0800

    build: compileSdk downTo 33

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.m3u.androidApp"
-    compileSdk = 34
+    compileSdk = 33
     defaultConfig {
         applicationId = "com.m3u.androidApp"
         minSdk = 26
