8016c9e597b2f0ecd36555a42d1054c4a904dd2e
oxy <sortby@qq.com>
2023-10-06 12:28:45 +0800

    feat: UBoolean?.orUnspecified

--- core/src/main/java/com/m3u/core/unspecified/UBoolean.kt
+++ core/src/main/java/com/m3u/core/unspecified/UBoolean.kt
@@ -6,4 +6,6 @@ enum class UBoolean {
 
 val Boolean.ub: UBoolean get() = if (this) UBoolean.True else UBoolean.False
 
+val UBoolean?.orUnspecified: UBoolean get() = this ?: UBoolean.Unspecified
+
 val UBoolean.isUnspecified: Boolean get() = this == UBoolean.Unspecified
