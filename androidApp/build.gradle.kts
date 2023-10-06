c545acf01fb673578bbee2dbcf8824a56e43630b
oxy <sortby@qq.com>
2023-10-06 11:26:38 +0800

    upgrade: 1.12.0-rc01

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.m3u.androidApp"
         minSdk = 26
         targetSdk = 33
-        versionCode = 64
-        versionName = "1.12.0-beta07"
+        versionCode = 65
+        versionName = "1.12.0-rc01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArguments["androidx.benchmark.profiling.mode"] = "MethodTracing"
