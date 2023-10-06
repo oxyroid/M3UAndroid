6f515c9cf9e8aac5cb1096daed974944684e8c35
oxy <sortby@qq.com>
2023-10-06 12:30:08 +0800

    feat: UBoolean.actual

--- core/src/main/java/com/m3u/core/unspecified/UBoolean.kt
+++ core/src/main/java/com/m3u/core/unspecified/UBoolean.kt
@@ -6,6 +6,13 @@ enum class UBoolean {
 
 val Boolean.ub: UBoolean get() = if (this) UBoolean.True else UBoolean.False
 
+val UBoolean.actual: Boolean?
+    get() = when (this) {
+        UBoolean.True -> true
+        UBoolean.False -> false
+        UBoolean.Unspecified -> null
+    }
+
 val UBoolean?.orUnspecified: UBoolean get() = this ?: UBoolean.Unspecified
 
 val UBoolean.isUnspecified: Boolean get() = this == UBoolean.Unspecified
