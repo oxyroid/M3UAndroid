b352cc5614c4bfb0449d8d104637d5040d78b8d0
oxy <sortby@qq.com>
2023-11-07 17:18:55 +0800

    upgrade: 1.12.7 (73)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.m3u.androidApp"
         minSdk = 26
         targetSdk = 33
-        versionCode = 72
-        versionName = "1.12.6"
+        versionCode = 73
+        versionName = "1.12.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArguments["androidx.benchmark.profiling.mode"] = "MethodTracing"
