dcf9429eb35b27c352ab54e08e45de1c385b71eb
oxy <sortby@qq.com>
2023-11-29 19:46:52 +0800

    feat: animated player shutter color.

--- ui/src/main/java/com/m3u/ui/Player.kt
+++ ui/src/main/java/com/m3u/ui/Player.kt
@@ -2,20 +2,28 @@ package com.m3u.ui
 
 import android.view.ViewGroup
 import androidx.annotation.OptIn
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.ui.AspectRatioFrameLayout
 import androidx.media3.ui.PlayerView
 import com.m3u.core.annotation.ClipMode
+import com.m3u.material.ktx.ifUnspecified
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlin.time.Duration.Companion.milliseconds
@@ -64,12 +72,22 @@ fun rememberPlayerState(
 @Composable
 fun Player(
     state: PlayerState,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    startColor: Color = Color.Unspecified,
 ) {
     val player = state.player
     val url = state.url
     val keepScreenOn = state.keepScreenOn
     val clipMode = state.clipMode
+    var actualColor by remember(startColor) { mutableStateOf(startColor) }
+    val currentShutterColor by animateColorAsState(
+        targetValue = actualColor.ifUnspecified { Color.Black },
+        label = "player-shutter-color",
+        animationSpec = tween(durationMillis = 800, delayMillis = 400)
+    )
+    LaunchedEffect(startColor) {
+        actualColor = Color.Black
+    }
 
     AndroidView(
         factory = { context ->
@@ -79,12 +97,13 @@ fun Player(
                     ViewGroup.LayoutParams.MATCH_PARENT,
                     ViewGroup.LayoutParams.MATCH_PARENT
                 )
-                setShutterBackgroundColor(0xffffff)
+                setShutterBackgroundColor(currentShutterColor.toArgb())
             }
         },
         modifier = modifier
     ) { view ->
         view.apply {
+            setShutterBackgroundColor(currentShutterColor.toArgb())
             setPlayer(player)
             setKeepScreenOn(keepScreenOn)
             resizeMode = when (clipMode) {
