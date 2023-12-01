6ea17c81593a91019215963dcf247a4d5d1c2d50
oxy <sortby@qq.com>
2023-12-01 22:34:51 +0800

    upgrade: 1.13.1 (80)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.m3u.androidApp"
         minSdk = 26
         targetSdk = 33
-        versionCode = 79
-        versionName = "1.13.0"
+        versionCode = 80
+        versionName = "1.13.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArguments["androidx.benchmark.profiling.mode"] = "MethodTracing"
