a34ae52e028371d8a59d1e5904eaee6b513b64ea
oxy <sortby@qq.com>
2023-12-06 15:56:35 +0800

    fix: add exoplayer-smoothscreaming.

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation(libs.androidx.media3.media3.exoplayer.dash)
     implementation(libs.androidx.media3.media3.exoplayer.hls)
     implementation(libs.androidx.media3.media3.exoplayer.rtsp)
+    implementation(libs.androidx.media3.media3.exoplayer.smoothstreaming)
     implementation(libs.androidx.media3.media3.session)
     implementation(libs.androidx.media3.media3.datasource.rtmp)
     implementation(libs.androidx.media3.media3.datasource.okhttp)
