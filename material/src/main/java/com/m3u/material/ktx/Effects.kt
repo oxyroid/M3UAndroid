07507e6aa087033fa2f2dc45347c1538f8c96452
oxy <sortby@qq.com>
2024-01-04 15:35:31 +0800

    build: adjust duration of border-effects.

--- material/src/main/java/com/m3u/material/ktx/Effects.kt
+++ material/src/main/java/com/m3u/material/ktx/Effects.kt
@@ -2,6 +2,7 @@ package com.m3u.material.ktx
 
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.border
 import androidx.compose.foundation.interaction.InteractionSource
 import androidx.compose.material3.MaterialTheme
@@ -12,6 +13,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.Dp
+import com.m3u.material.model.LocalDuration
 import com.m3u.material.model.LocalSpacing
 
 @Composable
@@ -29,14 +31,17 @@ fun Modifier.interactionBorder(
     width: Dp = Dp.Unspecified,
     shape: Shape = RectangleShape
 ): Modifier = interaction(type, source) { visible ->
+    val duration = LocalDuration.current
     val actualColor = color.ifUnspecified { InteractionDefaults.BorderColor }
     val actualDp = width.ifUnspecified { InteractionDefaults.BorderWidth }
     val currentColor by animateColorAsState(
         targetValue = if (visible) actualColor else Color.Transparent,
+        animationSpec = tween(duration.medium),
         label = "interaction-border-color"
     )
     val currentWidth by animateDpAsState(
         targetValue = if (visible) actualDp else Dp.Hairline,
+        animationSpec = tween(duration.medium),
         label = "interaction-border-width"
     )
     border(
