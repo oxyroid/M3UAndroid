139c8692ad79015a3eb73d94a728dcba067045c4
oxy <sortby@qq.com>
2024-02-01 20:49:23 +0800

    fix: cache okhttp error.

--- data/src/main/java/com/m3u/data/api/ApiModule.kt
+++ data/src/main/java/com/m3u/data/api/ApiModule.kt
@@ -5,18 +5,22 @@ package com.m3u.data.api
 import com.charleskorn.kaml.Yaml
 import com.charleskorn.kaml.YamlConfiguration
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import com.m3u.core.architecture.pref.impl.SnapshotPref
+import com.m3u.core.architecture.logger.Logger
+import com.m3u.data.Certs
+import com.m3u.data.SSL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.json.Json
+import okhttp3.Authenticator
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
+import okhttp3.Protocol
+import okhttp3.Response
+import okhttp3.ResponseBody.Companion.toResponseBody
 import retrofit2.Retrofit
 import retrofit2.create
-import java.time.Duration
-import java.time.temporal.ChronoUnit
 import javax.inject.Singleton
 
 @Module
@@ -25,13 +29,27 @@ internal object ApiModule {
     @Provides
     @Singleton
     fun provideOkhttpClient(
-        pref: SnapshotPref
+        logger: Logger
     ): OkHttpClient {
-        val connectTimeout = pref.connectTimeout
-        val duration = Duration.of(connectTimeout, ChronoUnit.MILLIS)
         return OkHttpClient.Builder()
-            .connectTimeout(duration)
-            .callTimeout(duration)
+            .authenticator(Authenticator.JAVA_NET_AUTHENTICATOR)
+            .addInterceptor { chain ->
+                val request = chain.request()
+                try {
+                    chain.proceed(request)
+                } catch (e: Exception) {
+                    logger.log(e)
+                    Response.Builder()
+                        .request(request)
+                        .protocol(Protocol.HTTP_1_1)
+                        .code(999)
+                        .message(e.message.orEmpty())
+                        .body("{${e}}".toResponseBody(null))
+                        .build()
+                }
+            }
+            .sslSocketFactory(SSL.TLSTrustAll.socketFactory, Certs.TrustAll)
+            .hostnameVerifier { _, _ -> true }
             .build()
     }
 
