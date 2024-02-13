9abf47cca88e8422c4795c4b2275941bdce5fa50
oxy <sortby@qq.com>
2024-02-13 21:08:40 +0800

    fix: foreground info for works.

--- data/src/main/java/com/m3u/data/work/BackupWorker.kt
+++ data/src/main/java/com/m3u/data/work/BackupWorker.kt
@@ -1,20 +1,23 @@
 package com.m3u.data.work
 
+import android.app.Notification
 import android.content.Context
 import android.net.Uri
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
+import androidx.work.ForegroundInfo
 import androidx.work.WorkerParameters
+import com.m3u.data.R
 import com.m3u.data.repository.PlaylistRepository
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 
 @HiltWorker
 class BackupWorker @AssistedInject constructor(
-    @Assisted appContext: Context,
+    @Assisted private val context: Context,
     @Assisted params: WorkerParameters,
     private val playlistRepository: PlaylistRepository
-) : CoroutineWorker(appContext, params) {
+) : CoroutineWorker(context, params) {
     private val uri = inputData.getString(INPUT_URI)?.let { Uri.parse(it) }
     override suspend fun doWork(): Result {
         uri ?: return Result.failure()
@@ -26,7 +29,22 @@ class BackupWorker @AssistedInject constructor(
         return Result.success()
     }
 
+    override suspend fun getForegroundInfo(): ForegroundInfo {
+        return ForegroundInfo(NOTIFICATION_ID, createNotification())
+    }
+
+    private fun createNotification(): Notification {
+        return Notification.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.round_file_download_24)
+            .setContentTitle("Backing up")
+            .setContentText("Please wait")
+            .build()
+    }
+
+
     companion object {
+        private const val CHANNEL_ID = "subscribe_channel"
+        private const val NOTIFICATION_ID = 1225
         const val TAG = "backup"
         const val INPUT_URI = "uri"
     }
--- data/src/main/java/com/m3u/data/work/RestoreWorker.kt
+++ data/src/main/java/com/m3u/data/work/RestoreWorker.kt
@@ -1,20 +1,23 @@
 package com.m3u.data.work
 
+import android.app.Notification
 import android.content.Context
 import android.net.Uri
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
+import androidx.work.ForegroundInfo
 import androidx.work.WorkerParameters
+import com.m3u.data.R
 import com.m3u.data.repository.PlaylistRepository
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 
 @HiltWorker
 class RestoreWorker @AssistedInject constructor(
-    @Assisted appContext: Context,
+    @Assisted private val context: Context,
     @Assisted params: WorkerParameters,
     private val playlistRepository: PlaylistRepository
-) : CoroutineWorker(appContext, params) {
+) : CoroutineWorker(context, params) {
 
     private val uri = inputData.getString(INPUT_URI)?.let { Uri.parse(it) }
 
@@ -27,8 +30,23 @@ class RestoreWorker @AssistedInject constructor(
         }
         return Result.success()
     }
+    override suspend fun getForegroundInfo(): ForegroundInfo {
+        return ForegroundInfo(NOTIFICATION_ID, createNotification())
+    }
+
+    private fun createNotification(): Notification {
+        return Notification.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.round_file_download_24)
+            .setContentTitle("Backing up")
+            .setContentText("Please wait")
+            .build()
+    }
+
+
 
     companion object {
+        private const val CHANNEL_ID = "subscribe_channel"
+        private const val NOTIFICATION_ID = 1226
         const val TAG = "restore"
         const val INPUT_URI = "uri"
     }
