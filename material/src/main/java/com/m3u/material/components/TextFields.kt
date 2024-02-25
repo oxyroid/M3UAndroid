cd8de37c6eee836970e7ad5252e5564b172dd3ac
oxy <sortby@qq.com>
2024-02-25 14:49:36 +0800

    feat: tv text-field.

--- material/src/main/java/com/m3u/material/components/TextFields.kt
+++ material/src/main/java/com/m3u/material/components/TextFields.kt
@@ -2,9 +2,12 @@
 
 package com.m3u.material.components
 
+import android.view.KeyEvent
 import androidx.activity.compose.BackHandler
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsFocusedAsState
@@ -28,14 +31,20 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -46,8 +55,13 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.tv.material3.Border
+import androidx.tv.material3.ClickableSurfaceDefaults
+import androidx.tv.material3.LocalContentColor
+import androidx.tv.material3.Surface
 import com.m3u.material.ktx.InteractionType
 import com.m3u.material.ktx.interactionBorder
+import com.m3u.material.ktx.isTelevision
 
 @Composable
 fun TextField(
@@ -168,122 +182,239 @@ fun PlaceholderField(
     icon: ImageVector? = null,
     onValueChange: (String) -> Unit = {},
 ) {
-    val focusManager = LocalFocusManager.current
-    val interactionSource = remember { MutableInteractionSource() }
-    val focus by interactionSource.collectIsFocusedAsState()
+    if (!isTelevision()) {
+        val focusManager = LocalFocusManager.current
+        val interactionSource = remember { MutableInteractionSource() }
+        val focus by interactionSource.collectIsFocusedAsState()
 
-    BackHandler(focus) {
-        focusManager.clearFocus()
-    }
+        BackHandler(focus) {
+            focusManager.clearFocus()
+        }
 
-    val fontSize = TextFieldDefaults.MinimizeLabelFontSize
+        val fontSize = TextFieldDefaults.MinimizeLabelFontSize
 
-    val theme = MaterialTheme.colorScheme
-    CompositionLocalProvider(
-        LocalTextSelectionColors provides TextSelectionColors(
-            handleColor = theme.primary,
-            backgroundColor = theme.primary.copy(alpha = 0.45f)
-        )
-    ) {
-        BasicTextField(
-            value = text,
-            singleLine = singleLine,
-            enabled = enabled,
-            textStyle = TextStyle(
-                fontFamily = MaterialTheme.typography.bodyMedium.fontFamily,
-                fontSize = fontSize,
-                color = contentColor,
-                fontWeight = fontWeight
-            ),
-            onValueChange = {
-                onValueChange(it)
-            },
-            keyboardActions = keyboardActions ?: KeyboardActions(
-                onDone = { focusManager.clearFocus() },
-                onNext = { focusManager.moveFocus(FocusDirection.Down) },
-                onSearch = { focusManager.clearFocus() }
-            ),
-            keyboardOptions = KeyboardOptions(
-                keyboardType = keyboardType,
-                imeAction = imeAction
-            ),
-            interactionSource = interactionSource,
-            modifier = modifier.fillMaxWidth(),
-            readOnly = readOnly,
-            cursorBrush = SolidColor(contentColor.copy(.35f)),
-            decorationBox = { innerTextField ->
-                Row(
-                    modifier = Modifier
-                        .clip(shape)
-                        .background(backgroundColor)
-                        .interactionBorder(
-                            type = InteractionType.PRESS,
-                            source = interactionSource,
-                            shape = shape
-                        ),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    icon?.let { icon ->
-                        Icon(
-                            modifier = Modifier
-                                .size(56.dp)
-                                .padding(15.dp),
-                            imageVector = icon,
-                            contentDescription = null,
-                            tint = contentColor
-                        )
-                    }
-
-                    Box(
-                        Modifier
+        val theme = MaterialTheme.colorScheme
+        CompositionLocalProvider(
+            LocalTextSelectionColors provides TextSelectionColors(
+                handleColor = theme.primary,
+                backgroundColor = theme.primary.copy(alpha = 0.45f)
+            )
+        ) {
+            BasicTextField(
+                value = text,
+                singleLine = singleLine,
+                enabled = enabled,
+                textStyle = TextStyle(
+                    fontFamily = MaterialTheme.typography.bodyMedium.fontFamily,
+                    fontSize = fontSize,
+                    color = contentColor,
+                    fontWeight = fontWeight
+                ),
+                onValueChange = {
+                    onValueChange(it)
+                },
+                keyboardActions = keyboardActions ?: KeyboardActions(
+                    onDone = { focusManager.clearFocus() },
+                    onNext = { focusManager.moveFocus(FocusDirection.Down) },
+                    onSearch = { focusManager.clearFocus() }
+                ),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = keyboardType,
+                    imeAction = imeAction
+                ),
+                interactionSource = interactionSource,
+                modifier = modifier.fillMaxWidth(),
+                readOnly = readOnly,
+                cursorBrush = SolidColor(contentColor.copy(.35f)),
+                decorationBox = { innerTextField ->
+                    Row(
+                        modifier = Modifier
+                            .clip(shape)
+                            .background(backgroundColor)
                             .interactionBorder(
                                 type = InteractionType.PRESS,
                                 source = interactionSource,
                                 shape = shape
-                            )
-                            .fillMaxWidth()
-                            .defaultMinSize(minHeight = 56.dp)
-                            .padding(
-                                start = if (icon == null) 15.dp else 0.dp,
-                                end = 15.dp
                             ),
-                        contentAlignment = Alignment.CenterStart
+                        verticalAlignment = Alignment.CenterVertically
                     ) {
-                        val hasText = text.isNotEmpty()
-
-                        val animPlaceholder: Dp by animateDpAsState(
-                            if (focus || hasText) (-10).dp else 0.dp,
-                            label = "placeholder-translation-y"
-                        )
-                        val animPlaceHolderFontSize: Float by animateFloatAsState(
-                            targetValue = if (focus || hasText) 12f else 14f,
-                            label = "placeholder-font-size"
-                        )
-
-                        Text(
-                            modifier = Modifier
-                                .graphicsLayer {
-                                    translationY = animPlaceholder.toPx()
-                                },
-                            text = placeholder,
-                            color = contentColor.copy(alpha = .35f),
-                            fontSize = animPlaceHolderFontSize.sp,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
-                            fontWeight = FontWeight.SemiBold
-                        )
+                        icon?.let { icon ->
+                            Icon(
+                                modifier = Modifier
+                                    .size(56.dp)
+                                    .padding(15.dp),
+                                imageVector = icon,
+                                contentDescription = null,
+                                tint = contentColor
+                            )
+                        }
 
                         Box(
                             Modifier
-                                .padding(top = -animPlaceholder)
+                                .interactionBorder(
+                                    type = InteractionType.PRESS,
+                                    source = interactionSource,
+                                    shape = shape
+                                )
                                 .fillMaxWidth()
-                                .heightIn(18.dp),
+                                .defaultMinSize(minHeight = 56.dp)
+                                .padding(
+                                    start = if (icon == null) 15.dp else 0.dp,
+                                    end = 15.dp
+                                ),
+                            contentAlignment = Alignment.CenterStart
                         ) {
-                            innerTextField()
+                            val hasText = text.isNotEmpty()
+
+                            val animPlaceholder: Dp by animateDpAsState(
+                                if (focus || hasText) (-10).dp else 0.dp,
+                                label = "placeholder-translation-y"
+                            )
+                            val animPlaceHolderFontSize: Float by animateFloatAsState(
+                                targetValue = if (focus || hasText) 12f else 14f,
+                                label = "placeholder-font-size"
+                            )
+
+                            Text(
+                                modifier = Modifier
+                                    .graphicsLayer {
+                                        translationY = animPlaceholder.toPx()
+                                    },
+                                text = placeholder,
+                                color = contentColor.copy(alpha = .35f),
+                                fontSize = animPlaceHolderFontSize.sp,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                                fontWeight = FontWeight.SemiBold
+                            )
+
+                            Box(
+                                Modifier
+                                    .padding(top = -animPlaceholder)
+                                    .fillMaxWidth()
+                                    .heightIn(18.dp),
+                            ) {
+                                innerTextField()
+                            }
                         }
                     }
                 }
-            }
+            )
+        }
+    } else {
+        TvTextFieldImpl(
+            value = text,
+            onValueChange = onValueChange,
+            placeholder = placeholder,
+            shape = shape,
+            modifier = modifier
+        )
+    }
+
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+private fun TvTextFieldImpl(
+    value: String,
+    onValueChange: (String) -> Unit,
+    placeholder: String,
+    modifier: Modifier = Modifier,
+    shape: Shape = RectangleShape,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+) {
+    val focusRequester = remember { FocusRequester() }
+    val focusManager = LocalFocusManager.current
+    val interactionSource = remember { MutableInteractionSource() }
+    val isFocus by interactionSource.collectIsFocusedAsState()
+    Surface(
+        shape = ClickableSurfaceDefaults.shape(shape),
+        scale = ClickableSurfaceDefaults.scale(focusedScale = 1f),
+        colors = ClickableSurfaceDefaults.colors(
+            containerColor = androidx.tv.material3.MaterialTheme.colorScheme.inverseOnSurface,
+            focusedContainerColor = androidx.tv.material3.MaterialTheme.colorScheme.inverseOnSurface,
+            pressedContainerColor = androidx.tv.material3.MaterialTheme.colorScheme.inverseOnSurface,
+            focusedContentColor = androidx.tv.material3.MaterialTheme.colorScheme.onSurface,
+            pressedContentColor = androidx.tv.material3.MaterialTheme.colorScheme.onSurface
+        ),
+        border = ClickableSurfaceDefaults.border(
+            focusedBorder = Border(
+                border = BorderStroke(
+                    width = if (isFocus) 2.dp else 1.dp,
+                    color = animateColorAsState(
+                        targetValue = if (isFocus) androidx.tv.material3.MaterialTheme.colorScheme.primary
+                        else androidx.tv.material3.MaterialTheme.colorScheme.border, label = ""
+                    ).value
+                ),
+                shape = shape
+            )
+        ),
+        tonalElevation = 2.dp,
+        modifier = modifier
+            .padding(horizontal = 8.dp)
+            .padding(top = 8.dp),
+        onClick = { focusRequester.requestFocus() }
+    ) {
+        BasicTextField(
+            value = value,
+            onValueChange = onValueChange,
+            decorationBox = {
+                Box(
+                    modifier = Modifier
+                        .padding(vertical = 16.dp)
+                        .padding(start = 20.dp),
+                ) {
+                    it()
+                    if (value.isEmpty()) {
+                        androidx.tv.material3.Text(
+                            modifier = Modifier.graphicsLayer { alpha = 0.6f },
+                            text = placeholder,
+                            style = androidx.tv.material3.MaterialTheme.typography.titleSmall
+                        )
+                    }
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    vertical = 4.dp,
+                    horizontal = 8.dp
+                )
+                .focusRequester(focusRequester)
+                .onKeyEvent {
+                    if (it.nativeKeyEvent.action == KeyEvent.ACTION_UP) {
+                        when (it.nativeKeyEvent.keyCode) {
+                            KeyEvent.KEYCODE_DPAD_DOWN -> {
+                                focusManager.moveFocus(FocusDirection.Down)
+                            }
+
+                            KeyEvent.KEYCODE_DPAD_UP -> {
+                                focusManager.moveFocus(FocusDirection.Up)
+                            }
+
+                            KeyEvent.KEYCODE_BACK -> {
+                                focusManager.moveFocus(FocusDirection.Exit)
+                            }
+                        }
+                    }
+                    true
+                },
+            cursorBrush = Brush.verticalGradient(
+                colors = listOf(
+                    LocalContentColor.current,
+                    LocalContentColor.current,
+                )
+            ),
+            keyboardOptions = KeyboardOptions(
+                autoCorrect = false,
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = keyboardActions,
+            maxLines = 1,
+            interactionSource = interactionSource,
+            textStyle = androidx.tv.material3.MaterialTheme.typography.titleSmall.copy(
+                color = androidx.tv.material3.MaterialTheme.colorScheme.onSurface
+            )
         )
     }
 }
