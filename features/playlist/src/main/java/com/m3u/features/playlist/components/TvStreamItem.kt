b4a567beb02f2dde356e87eb1508b7f4eb74a46d
oxy <sortby@qq.com>
2024-04-26 14:45:31 +0800

    fix: tv playlist gallery item box.

--- features/playlist/src/main/java/com/m3u/features/playlist/components/TvStreamItem.kt
+++ features/playlist/src/main/java/com/m3u/features/playlist/components/TvStreamItem.kt
@@ -1,9 +1,10 @@
 package com.m3u.features.playlist.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -11,23 +12,29 @@ import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.BrokenImage
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.tv.material3.Border
 import androidx.tv.material3.Card
 import androidx.tv.material3.CardDefaults
 import androidx.tv.material3.Glow
+import androidx.tv.material3.MaterialTheme
 import androidx.tv.material3.Text
 import coil.compose.SubcomposeAsyncImage
+import coil.request.ImageRequest
 import com.m3u.core.architecture.preferences.LocalPreferences
 import com.m3u.data.database.model.Stream
 import com.m3u.material.components.CircularProgressIndicator
 import com.m3u.material.components.Icon
 import com.m3u.material.ktx.thenIf
 import com.m3u.material.model.LocalSpacing
+import coil.size.Size as CoilSize
 
 @Composable
 internal fun TvStreamItem(
@@ -37,6 +44,7 @@ internal fun TvStreamItem(
     onLongClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val context = LocalContext.current
     val preferences = LocalPreferences.current
     val spacing = LocalSpacing.current
 
@@ -56,13 +64,15 @@ internal fun TvStreamItem(
             scale = 0.95f,
             focusedScale = 1.1f
         ),
+        border = CardDefaults.border(
+            if (stream.favourite) Border(
+                BorderStroke(3.dp, MaterialTheme.colorScheme.border),
+            )
+            else Border.None
+        ),
         modifier = Modifier
             .thenIf(!noPictureMode) {
-                Modifier
-                    .height(128.dp)
-                    .aspectRatio(
-                        if (!isVodOrSeriesPlaylist) 4 / 3f else 2 / 3f
-                    )
+                Modifier.height(128.dp)
             }
             .then(modifier)
     ) {
@@ -70,7 +80,6 @@ internal fun TvStreamItem(
             contentAlignment = Alignment.Center,
             modifier = Modifier.fillMaxSize()
         ) {
-
             if (!isCoverExisted || noPictureMode) {
                 Text(
                     text = stream.title,
@@ -82,8 +91,13 @@ internal fun TvStreamItem(
                 )
             } else {
                 SubcomposeAsyncImage(
-                    model = stream.cover,
-                    contentScale = ContentScale.FillWidth,
+                    model = remember(stream.cover) {
+                        ImageRequest.Builder(context)
+                            .data(stream.cover)
+                            .size(CoilSize.ORIGINAL)
+                            .build()
+                    },
+                    contentScale = ContentScale.FillHeight,
                     contentDescription = stream.title,
                     loading = {
                         Column(
@@ -118,10 +132,9 @@ internal fun TvStreamItem(
                             )
                         }
                     },
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier.fillMaxHeight()
                 )
             }
         }
     }
 }
-
