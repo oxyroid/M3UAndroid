dd0985282ced4b11caa325b5ff6ce5ab4afc3c62
oxy <sortby@qq.com>
2024-05-15 14:16:32 +0800

    upgrade: 1.14.0-beta12(138)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.m3u.androidApp"
         minSdk = 26
         targetSdk = 33
-        versionCode = 137
-        versionName = "1.14.0-beta11"
+        versionCode = 138
+        versionName = "1.14.0-beta12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArguments["androidx.benchmark.profiling.mode"] = "MethodTracing"
