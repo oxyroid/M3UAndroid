0a276323854ce568f5afae9ce914ee68862c4b63
oxy <sortby@qq.com>
2024-07-21 21:11:56 +0800

    Upgrade haze and leakcanary.

--- material/src/main/java/com/m3u/material/ktx/Blurs.kt
+++ material/src/main/java/com/m3u/material/ktx/Blurs.kt
@@ -1,7 +1,6 @@
 package com.m3u.material.ktx
 
 import androidx.compose.animation.animateColorAsState
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
