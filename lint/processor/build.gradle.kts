a53f290afc51dce75cc1ac965c87bb5b33049644
oxy <sortby@qq.com>
2026-05-04 01:45:48 +0800

    Avoid KSP bootstrap in lint processor

--- lint/processor/build.gradle.kts
+++ lint/processor/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     kotlin("jvm")
-    alias(libs.plugins.com.google.devtools.ksp)
 }
 
 kotlin {
@@ -10,18 +9,10 @@ kotlin {
     }
 }
 
-ksp {
-    arg("autoserviceKsp.verify", "true")
-    arg("autoserviceKsp.verbose", "true")
-}
-
 dependencies {
     implementation(project(":lint:annotation"))
 
     implementation(libs.symbol.processing.api)
     implementation(libs.kotlinpoet)
     implementation(libs.kotlinpoet.ksp)
-    implementation(libs.auto.service.annotations)
-
-    ksp(libs.auto.service.ksp)
 }
--- lint/processor/src/main/java/com/m3u/processor/likable/LikableSymbolProcessorProvider.kt
+++ lint/processor/src/main/java/com/m3u/processor/likable/LikableSymbolProcessorProvider.kt
@@ -1,11 +1,9 @@
 package com.m3u.processor.likable
 
-import com.google.auto.service.AutoService
 import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 import com.google.devtools.ksp.processing.SymbolProcessorProvider
 
-@AutoService(SymbolProcessorProvider::class)
 class LikableSymbolProcessorProvider : SymbolProcessorProvider {
     override fun create(environment: SymbolProcessorEnvironment): SymbolProcessor {
         return LikableSymbolProcessor(
