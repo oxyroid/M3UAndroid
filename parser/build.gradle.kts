35216e487941541c7d6d0b22df79104341f639e1
oxy <oxyroid@outlook.com>
2026-05-19 17:16:31 +0000

    chore: convert parser module to git submodule

--- parser/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    kotlin("jvm") version "2.3.0" apply false
-}
-
-subprojects {
-    group = "dev.oxyroid.parser"
-    version = "1.0-SNAPSHOT"
-}
--- parser/protocol/src/main/kotlin/dev/oxyroid/parser/protocol/Parser.kt
+++
@@ -1,5 +0,0 @@
-package dev.oxyroid.parser.protocol
-
-interface Parser<in Input, out Output> {
-    fun parse(input: Input): Sequence<Output>
-}
